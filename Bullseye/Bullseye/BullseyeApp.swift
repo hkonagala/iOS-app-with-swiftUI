a7c12836859c858adcadb5cfdef66823ba3dda6d
Harika Konagala <pair+harika.konagala@omadahealth.com>
2021-01-21 15:17:50 -0800

    added new xcode project

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by harika.konagala on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
